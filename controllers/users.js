708d4e64feebe8ced4ebeb110a6e197d1d3bce7f
New Greivin <grarguedasgu@est.utn.ac.cr>
2026-06-04 14:59:25 -0600

    Actualizacion archivo users.js
    Se cambio el mensaje enviado

--- controllers/users.js
+++ controllers/users.js
@@ -6,7 +6,7 @@ const getMethod=(req=request, res=response)=>{
     const {nombre,universidad}=req.query;
     res.json({
         OK:"200",
-        msj:"Mesaje GET devuelto desde el controlador ",
+        msj:"Mensaje del repositorio DEV",
         nombre,
         universidad:"UTN",
         modo:"Desarrollo",
